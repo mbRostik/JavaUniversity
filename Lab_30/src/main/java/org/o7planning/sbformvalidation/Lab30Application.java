1fe7a6ee5fb25603625a75d15abc487112c6fd44
Rost Daskaliuk <147745141+mbRostik@users.noreply.github.com>
2023-11-20 13:49:10 +0200

    Add files via upload

---
+++ Lab_30/src/main/java/org/o7planning/sbformvalidation/Lab30Application.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbformvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab30Application.class, args);
+	}
+
+}
---
+++ Lab_30/src/test/java/org/o7planning/sbformvalidation/Lab30ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbformvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab30ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
