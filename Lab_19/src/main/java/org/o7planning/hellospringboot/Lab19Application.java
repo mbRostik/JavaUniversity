191cfd33f5fa39735adf1b3e92d0be32e27710a2
Rost Daskaliuk <147745141+mbRostik@users.noreply.github.com>
2023-11-20 13:40:52 +0200

    Add files via upload

---
+++ Lab_19/src/main/java/org/o7planning/hellospringboot/Lab19Application.java
@@ -0,0 +1,11 @@
+package org.o7planning.hellospringboot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab19Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Lab19Application.class, args);
+    }
+    
+}
