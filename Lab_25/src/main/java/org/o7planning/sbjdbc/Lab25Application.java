78b432bb13f322f55991b9fc929d18314a2bc076
Rost Daskaliuk <147745141+mbRostik@users.noreply.github.com>
2023-11-20 13:44:17 +0200

    Add files via upload

---
+++ Lab_25/src/main/java/org/o7planning/sbjdbc/Lab25Application.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab25Application.class, args);
+	}
+
+}
